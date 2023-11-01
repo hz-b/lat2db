bd26c831bef20cdc792eed7ff4dacc2a6d69052f
Waheed <wsulimankhail@gmail.com>
2023-11-01 12:20:51 +0100

    [TASK] kicked out thor_scsi dependecies. instead used lark parser and lark grammar from felix andreas project

--- scripts/export_lattice_to_db.py
+++ scripts/export_lattice_to_db.py
@@ -1,6 +1,8 @@
 from tools.export_to_json import export
 
-from thor_scsi.factory import parse_config_file
+from pathlib import Path
+from lark import Lark
+from tools.parser import MADXTransformer, parse
 
 def main():
     import os
@@ -16,8 +18,17 @@ def main():
         "b2_stduser_beamports_blm_tracy_corr.lat", #name of the lattice file you want to read todo: move to configuration instead of hardcoding
     )
 
-    config = parse_config_file(filename)
-    export(config)
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    with open(filename, 'r') as file:
+        lattice_content = file.read()
+    with (BASE_DIR / "tools/madx.lark").open() as file:
+        MADX_PARSER = Lark(file, parser="lalr", maybe_placeholders=True)
+        file.seek(0)
+    tree = MADX_PARSER.parse(lattice_content)
+    machine_data = MADXTransformer().transform(tree)
+    organized_dict = parse(machine_data)
+    variables = machine_data['variables']
+    export(organized_dict, variables)
 
 
 if __name__ == "__main__":
