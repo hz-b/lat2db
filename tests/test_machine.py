147424b271963560b0642c562b091c50cd4d95e8
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-11-22 14:54:51 +0100

    [Snapshot] deactivated not finished tests

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -1,30 +1,42 @@
+import pytest
+
 class TestMachine:
+    @pytest.mark.skip
     def test_add_drift(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_bending(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_sextupole(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_quadrupole(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_marker(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_horizontal_steerer(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_vertical_steerer(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_beam_position_monitor(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_cavity(self):
         assert False
 
+    @pytest.mark.skip
     def test_add_to_sequence(self):
         assert False
