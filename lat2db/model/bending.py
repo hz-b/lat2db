9123de325728e80ef7c597a900251151314275d6
Waheed <wsulimankhail@gmail.com>
2024-02-27 10:36:30 +0100

    [TASK] [TODO] dipole restructuring proposal adding up stream and down stream DRAFTED

--- lat2db/model/bending.py
+++ lat2db/model/bending.py
@@ -5,14 +5,43 @@ from .element import Element
 from .magnetic_element import MagneticElement
 from typing import Optional
 
+@dataclass
+class BendingGeometry:
+    """Geometyr of the bending magnet
+
+    Todo:
+        Compare entrance angle exit angle to the names chosen in mad-ng
+        entrance angle is beam use specific
+        please not in "design" or generic models these magnets could be "mirrored", then
+        exit becomes entrance and vice versa
+
+        Alternative suggestions
+
+        * down_stream
+        * up_stream
+
+        Side `A`: negative coordinate side
+        Side `B`: positive coordinate side
+
+        This idea above follows the naming convention of magnet connections at LHC.
+        For humans its typically eassier to say:
+
+           I put "side `A`" to the downstream side
+           
+    """
+    bending_angle: Optional[float] = 0.0
+    entrance_angle: Optional[float] = 0.0
+    exit_angle: Optional[float] = 0
+    #: full magnet height
+    gap: Optional[float] = None
 
 @dataclass
 class Bending(Element):
     element_properties: Optional[MagneticElement]=None
-    number_of_integration_steps: Optional[int] = None
-    bending_angle: Optional[float] = 0.0
-    entranceangle: Optional[float] = 0.0
-    exitangle: Optional[float] = 0
+    geometry : BendingGeometry
+    #: property of the machine or lattice or a user defined parameter
     Energy: Optional[float] = None
+    #: need to understand that value
+    #: should be something like end model
+    #: could be that it is very calculation engine specific
     fringeint1: Optional[float] = None
-    fullgap: Optional[float] = None
