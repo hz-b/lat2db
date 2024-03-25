d7749265eab5b3fe91a9813221a490144a0120da
Waheed <wsulimankhail@gmail.com>
2024-03-25 08:13:58 +0100

    cleaning unused lines

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,5 @@
 import sys
 
-
 from fastapi.staticfiles import StaticFiles
 
 sys.path.append('/Users/safiullahomar/lattice/lat2db')
@@ -9,8 +8,6 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from lat2db.controller import machine_controller, index_controller
 from lat2db import mongodb_url
-from lat2db.controller.machine_controller import router as machine_router
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
@@ -28,19 +25,18 @@ app.include_router(index_controller.router, tags=["index"], prefix="/index")
 
 app.mount("/", StaticFiles(directory="../UI/build", html=True))
 
+
 @app.on_event("startup")
 def startup_db_client():
     # use this if you want to write to the bessy server
     # app.mongodb_client = MongoClient("mongodb://mongodb.bessy.de:27017/")
-    app.mongodb_client = MongoClient(mongodb_url) # use this if you are writing to your local machine.
+    app.mongodb_client = MongoClient(mongodb_url)  # use this if you are writing to your local machine.
     app.database = app.mongodb_client["bessyii"]
 
 
 @app.on_event("shutdown")
 def shutdown_db_client():
     pass
-    # app.mongodb_client.close()
-
 
 
 @app.get("/healthcheck/")
