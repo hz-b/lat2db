acbf1848439443d04a9ed249a918a469cb5e937c
Waheed <wsulimankhail@gmail.com>
2024-03-27 09:58:12 +0100

    element position fixed

--- examples/pyat/element_position.py
+++ examples/pyat/element_position.py
@@ -1,5 +1,5 @@
 from lat2db.controller.get_machine_output import get_machine
 
-machine = get_machine(machine_id="65f96b620bdd4bc72d87dd93")
+machine = get_machine(machine_id="6602a1b4ea5f066ab6e502af")
 result = machine.retrieve_element_coordinate("Q5M1T1R")
 result
