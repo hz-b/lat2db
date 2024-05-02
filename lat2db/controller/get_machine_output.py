31aa8dc81a8f6342118b6e9499a78c5403a4d520
Waheed <wsulimankhail@gmail.com>
2024-05-02 14:59:39 +0200

    [TASK] consistency on pymongo usage. everywhere only use the one from config

--- lat2db/controller/get_machine_output.py
+++ lat2db/controller/get_machine_output.py
@@ -1,30 +1,37 @@
 import json
 from typing import List, Dict
+
 import jsons
 from bson import ObjectId
-from lat2db.db.mongo_repository import InitializeMongo
+from pymongo import MongoClient
+
+from lat2db import mongodb_url
 from lat2db.model.machine import Machine
 
-mongo_init = InitializeMongo()
+mongo_init = {'client': MongoClient(mongodb_url), 'db': MongoClient(mongodb_url)['bessyii']}
 
 
 def get_machine(machine_id: str) -> Machine:
-    machine = mongo_init.get_collection("machines").find_one({"_id": ObjectId(machine_id)})
+    machine = mongo_init['db']["machines"].find_one({"_id": ObjectId(machine_id)})
     return jsons.load(machine, Machine)
 
+
 def get_machine_as_json_from_db(machine_id: str):
-    if(machine_id is None ):
-        machine = mongo_init.get_collection("machines").find_one({})
+    if (machine_id is None):
+        machine = mongo_init['db']["machines"].find_one({})
         return machine
     else:
-        machine = mongo_init.get_collection("machines").find_one({"_id": ObjectId(machine_id)})
+        machine = mongo_init['db']["machines"].find_one({"_id": ObjectId(machine_id)})
         return machine
 
+
 def get_machine_as_json(file_name):
-    if(file_name is None ):
-        file_name ="bessyii_lattice_json.json"
+    if (file_name is None):
+        file_name = "bessyii_lattice_json.json"
     with open(file_name, 'r') as file:
         return json.load(file)
+
+
 def get_machine_element_list(machine: List[Dict], element_name: str):
     element_list = machine[f'{element_name}']
     return element_list
@@ -39,7 +46,7 @@ def filter_an_element(tags: List[str], element_list: List[Dict], element_name: s
 
 
 def filter_machine_element_by_tags(machine_id: str, element_name: str, tags: List[str]):
-    machine = mongo_init.get_collection("machines").find_one({"_id": ObjectId(machine_id)})
+    machine = mongo_init['db']["machines"].find_one({"_id": ObjectId(machine_id)})
     element_list = machine[f'{element_name}']
     return filter_an_element(tags, element_list, element_name)
 
