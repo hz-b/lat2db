f7b1abffe4cb5c8a0b07449bb6dbf4d149c2c89a
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-06-05 14:32:15 +0200

    [TASK] lat2db defined by environment variable

--- lat2db/__init__.py
+++ lat2db/__init__.py
@@ -1,4 +1,5 @@
 from ._config import mongodb_url as _mongodb_url
+
 mongodb_url = _mongodb_url(__name__)
 
-__all__ = ["bl", "controller", "model"]
+__all__ = ["bl", "controller", "model", "mongodb_url"]
