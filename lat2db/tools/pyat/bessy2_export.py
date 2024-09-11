202874dde003453435043f1344f3b7f039537022
safiullah omar <omar.safiullah@gmail.com>
2024-09-11 06:21:52 -0500

    cleaning the flask api

--- lat2db/tools/pyat/bessy2_export.py
+++ lat2db/tools/pyat/bessy2_export.py
@@ -1386,19 +1386,7 @@ def export_to_mongodb(lattice, mongodb_uri, database_name, collection_name):
     client.close()
 
 
-from flask import Flask, jsonify
-from pymongo import MongoClient
 
-app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')
-db = client['db1']
-collection = db['machines']
-
-
-@app.route('/api/elements', methods=['GET'])
-def get_elements():
-    elements = list(collection.find({}, {'_id': 0}))  # Exclude _id field
-    return jsonify(elements)
 
 
 if __name__ == '__main__':
