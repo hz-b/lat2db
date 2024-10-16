79b14276b254f74d4989c21af5118d5ed12e7981
Waheed <wsulimankhail@gmail.com>
2024-10-16 14:42:31 +0200

    no cavity phase for transfer line lattices

--- lat2db/controller/get_at_ring_output.py
+++ lat2db/controller/get_at_ring_output.py
@@ -8,8 +8,9 @@ def get_AT_ring_from_machine(machine : Machine):
     seq = factory(machine)
 
     ring = at.Lattice(seq, name='bessy2', periodicity=1, energy=1.7e9)
-    ring.enable_6d()  # Should 6D be default?
-    # Set main cavity phases
+
     if machine['closed']:
+        ring.enable_6d()  # Should 6D be default?
+    # Set main cavity phases
         ring.set_cavity_phase(cavpts='CAV*')
     return ring
