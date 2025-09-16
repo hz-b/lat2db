b34b314f32a574e01e8468fc206a521cb3c7eb4c
Waheed <wsulimankhail@gmail.com>
2025-09-16 13:29:15 +0200

    [TASK] mls new lattice

--- lat2db/controller/get_at_ring_output.py
+++ lat2db/controller/get_at_ring_output.py
@@ -7,7 +7,7 @@ from lat2db.tools.factories.pyat import factory
 def get_AT_ring_from_machine(machine : Machine):
     seq = factory(machine)
 
-    ring = at.Lattice(seq, name='bessy2', periodicity=1, energy=1.7e9)
+    ring = at.Lattice(seq, name='bessy2', periodicity=1, energy=629e6)
 
     if machine['closed']:
         ring.enable_6d()  # Should 6D be default?
