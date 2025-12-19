ab97d374bd9b0cbbefadc97d3682c11909fc3413
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-12-19 15:43:15 +0100

    [FIX] energy needs to be defined independently of the lattice
    Todo: Need to find out if it is needed for instaniating the (magnetic) lattice

--- lat2db/controller/get_at_ring_output.py
+++ lat2db/controller/get_at_ring_output.py
@@ -4,13 +4,18 @@ from lat2db.model.machine import Machine
 from lat2db.tools.factories.pyat import factory
 
 
-def get_AT_ring_from_machine(machine : Machine):
-    seq = factory(machine)
+def get_AT_ring_from_machine(machine : Machine, *, energy):
+    """
+    Todo:
+        eneregy should be part of config
+    """
+    seq = factory(machine, energy)
 
-    ring = at.Lattice(seq, name='bessy2', periodicity=1, energy=629e6)
+    ring = at.Lattice(seq, name='bessy2', periodicity=1, energy=energy)
 
     if machine['closed']:
-        ring.enable_6d()  # Should 6D be default?
-    # Set main cavity phases
+        # need to add this info in diff
+        ring.enable_6d()
+        # Set main cavity phases
         ring.set_cavity_phase(cavpts='CAV*')
     return ring
